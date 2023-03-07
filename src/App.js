1d0e0df9c568e22547cdfa63bf79abe27e6a903a
Bugra Unaltay <bugraunaltay@gmail.com>
2023-03-07 16:33:02 +0300

    Add code App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [quote, setQuote] = useState('');
+
+  const fetchQuote = async () => {
+    const response = await fetch('https://api.quotable.io/random');
+    const data = await response.json();
+    setQuote(data.content);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Random Quote Generator</h1>
+      <button onClick={fetchQuote}>Generate Quote</button>
+      <p>{quote}</p>
     </div>
   );
 }
