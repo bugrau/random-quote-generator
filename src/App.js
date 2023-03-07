a2a9464b3495cf0e6ccc57d4d982eb2a5cd0d573
Bugra Unaltay <bugraunaltay@gmail.com>
2023-03-07 17:08:30 +0300

    Stylized Version

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './index.css';
 
 function App() {
   const [quote, setQuote] = useState('');
@@ -10,11 +11,15 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Random Quote Generator</h1>
-      <button onClick={fetchQuote}>Generate Quote</button>
-      <p>{quote}</p>
+    <div class="flex items-center justify-center h-screen">
+    <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
+      <h1 class="text-3xl font-bold mb-4">Random Quote Generator</h1>
+      <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={fetchQuote}>Generate Quote</button>
+      <p class="text-lg mt-4">{quote}</p>
     </div>
+  </div>
+  
+
   );
 }
 
