0ad87fb60243de43726c9f92d62dd3743f431a83
Bugra Unaltay <bugraunaltay@gmail.com>
2023-03-07 17:28:37 +0300

    Add gradient color background

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ function App() {
 
   return (
     <div class="flex items-center justify-center h-screen">
-    <div class="bg-gray-100 p-4 rounded-lg shadow-lg text-center">
-      <h1 class="text-3xl font-bold mb-4">Random Quote Generator</h1>
-      <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={fetchQuote}>Generate Quote</button>
-      <p class="text-lg mt-4">{quote}</p>
+      <div class="bg-gradient-to-r from-green-200 to-green-300 p-4 rounded-lg shadow-lg text-center w-96 h-96">
+        <h1 class="text-3xl font-bold mb-4">Random Quote Generator</h1>
+        <button class="bg-gradient-to-r from-blue-400 to-blue-500 text-white font-bold py-2 px-4 rounded" onClick={fetchQuote}>Generate Quote</button>
+        <p class="text-lg mt-4">{quote}</p>
+      </div>
     </div>
-  </div>
   
 
   );
